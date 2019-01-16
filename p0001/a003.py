573728f3b264857b7f3e8474d5948dbb4987b2ba
wangluqing <Zhang12347>
2019-01-16 19:44:56 +0800

    003 weibo auto follow

--- p0001/a003.py
+++ p0001/a003.py
@@ -1,14 +1,17 @@
-#weibo auto login 模拟
+#weibo auto follow
 from selenium import webdriver
 from time import sleep
 
+
 driver = webdriver.Chrome()
-driver.get("http://weibo.com/")
-sleep(15)
+driver.get("https://weibo.com/u/1402977920")
+sleep (10)
+driver.find_element_by_xpath("//div[@node-type='focusLink']").click()
+sleep(5)
 driver.find_element_by_xpath("//input[@node-type='username']").send_keys("coonne")
-sleep(3)
+sleep(5)
 driver.find_element_by_xpath("//input[@node-type='password']").send_keys("Zhang12347")
-
+sleep(2)
 driver.find_element_by_xpath("//a[@node-type='submitBtn']").click()
-
-
+sleep(10)
+driver.find_element_by_xpath("//div[@node-type='focusLink']").click()
