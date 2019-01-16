ab2c5db7512a455c6f1b811d08c93e344227767c
coonn <coonne@qq.com>
2019-01-16 23:02:29 +0800

    003 增加静默运行

--- p0001/a003.py
+++ p0001/a003.py
@@ -1,9 +1,14 @@
 #weibo auto follow
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.chrome.options import Options
 
+#后台静默运行
+pro_option = Options()
+pro_option.add_argument('--headless')
+driver = webdriver.Chrome(chrome_options=pro_option)
 
-driver = webdriver.Chrome()
+#运行
 driver.get("https://weibo.com/u/1402977920")
 sleep (10)
 driver.find_element_by_xpath("//div[@node-type='focusLink']").click()
@@ -15,3 +20,6 @@ sleep(2)
 driver.find_element_by_xpath("//a[@node-type='submitBtn']").click()
 sleep(10)
 driver.find_element_by_xpath("//div[@node-type='focusLink']").click()
+
+#打印
+print (driver.title)
