c358caadbdb489dd0ce80b84d101e05e974ae1b9
wangluqing <Zhang12347>
2019-01-16 17:46:54 +0800

    weibo search and search click

---
+++ p0001/a001.py
@@ -0,0 +1,9 @@
+
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome()
+driver.get("http://weibo.com/")
+sleep(15)
+driver.find_element_by_xpath("//input[@node-type='searchInput']").send_keys("test")
+driver.find_element_by_xpath("//a[@node-type='searchSubmit']").click()
