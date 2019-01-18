bfb67b59c44f9839053518449f1ef162ec573142
wangluqing <Zhang12347>
2019-01-18 11:05:33 +0800

    a004 add like and forword

---
+++ p0001/a004.py
@@ -0,0 +1,40 @@
+#auto zhuanfa
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+#pro_option = Options()
+#pro_option.add_argument('--headless')
+#driver = webdriver.Chrome(chrome_options=pro_option)
+
+
+
+driver = webdriver.Chrome()
+#运行
+driver.get("https://weibo.com/yuritt?topnav=1&wvr=6&topsug=1&is_all=1")#进入博主主页
+sleep (10)
+driver.find_element_by_xpath("//div[@node-type='focusLink']").click()#点击关注
+sleep(5)
+driver.find_element_by_xpath("//input[@node-type='username']").send_keys("coonne")#弹出登录窗口输入账号
+sleep(5)
+driver.find_element_by_xpath("//input[@node-type='password']").send_keys("Zhang12347")#弹出登录窗口输入密码
+sleep(2)
+driver.find_element_by_xpath("//a[@node-type='submitBtn']").click()#弹出登录窗口点击登录按钮
+sleep(10)
+driver.find_element_by_xpath("//div[@node-type='focusLink']").click()#点击博主主页上的关注
+sleep(5)
+driver.find_element_by_xpath("//span[@node-type='like_status']").click()#点击第一条微博的LIKE按钮
+sleep(10)
+driver.find_element_by_xpath("//span[@node-type='forward_btn_text']").click()#第一条微博的转发按钮
+#sleep(5)
+#driver.find_element_by_xpath("//textarea[@node-type='textEl']").send_keys("可以可以")#弹出转发窗口输入内容
+#sleep(5)
+#driver.find_element_by_xpath("//input[@node-type='originInput']").click()#弹出转发窗口，勾选同时评论
+sleep(3)
+driver.find_element_by_xpath("//a[@node-type='submit']").click()#弹出转发窗口中转发按钮
+
+
+#打印
+print (driver.title)
