42f54704cc3d0d94dc9897569d798bd54e3a7116
wangluqing <Zhang12347>
2019-01-18 16:27:28 +0800

    a004 add like and forword

--- p0001/a004.py
+++ p0001/a004.py
@@ -13,7 +13,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome()
 #运行
-driver.get("https://weibo.com/yuritt?topnav=1&wvr=6&topsug=1&is_all=1")#进入博主主页
+driver.get("https://weibo.com/531456966?topnav=1&wvr=6&topsug=1&is_hot=1")#进入博主主页
+
 sleep (10)
 driver.find_element_by_xpath("//div[@node-type='focusLink']").click()#点击关注
 sleep(5)
@@ -26,14 +27,14 @@ sleep(10)
 driver.find_element_by_xpath("//div[@node-type='focusLink']").click()#点击博主主页上的关注
 sleep(5)
 driver.find_element_by_xpath("//span[@node-type='like_status']").click()#点击第一条微博的LIKE按钮
-sleep(10)
-driver.find_element_by_xpath("//span[@node-type='forward_btn_text']").click()#第一条微博的转发按钮
+#sleep(10)
+#driver.find_element_by_xpath("//span[@node-type='forward_btn_text']").click()#第一条微博的转发按钮
 #sleep(5)
 #driver.find_element_by_xpath("//textarea[@node-type='textEl']").send_keys("可以可以")#弹出转发窗口输入内容
 #sleep(5)
 #driver.find_element_by_xpath("//input[@node-type='originInput']").click()#弹出转发窗口，勾选同时评论
-sleep(3)
-driver.find_element_by_xpath("//a[@node-type='submit']").click()#弹出转发窗口中转发按钮
+#sleep(3)
+#driver.find_element_by_xpath("//a[@node-type='submit']").click()#弹出转发窗口中转发按钮
 
 
 #打印
