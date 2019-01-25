c09d2d6f4be3635862ceec2706811f461ec930ce
wangluqing <coonne@qq.com>
2019-01-25 16:13:22 +0800

    add a005 for mysql select

--- p0001/a004.py
+++ p0001/a004.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.chrome.options import Options
+from a005
 
 
 
--- tmp/selectdata.py
+++ tmp/selectdata.py
@@ -1,23 +1,22 @@
 import pymysql.cursors
+from time import sleep
 
-db = pymysql.connections.Connection(host='www.gytech.vip',user='gytech',password='wegmxn#e27bVR(',port=55944,database='gytech_main')
+
+db = pymysql.connections.Connection(host='www.gytech.vip', user='gytech', password='wegmxn#e27bVR(', port=55944, database='gytech_main')
+
+def lg_ps(acc, pwd):
+    print("%s , %s " %(acc, pwd))
+    pass
 
 try:
     with db.cursor() as cursor:
         sql1 = 'select * from wb_stat where wb_num >= 1 '
-        count1 = cursor.execute(sql1)
-
-
-        print(count1)
-
-        count2 = cursor.fetchall()
-
-        for i in count2:
-            print(i)
+        Opt1 = cursor.execute(sql1)
+        Opt2 = cursor.fetchall()
+        for i in Opt2:
+            lg_ps(i[1], i[2])
+            sleep(90)
         db.commit()
-        #for i in result:
-      #      print(i)
-       # db.commit()
 except:
     db.rollback()
 finally:
